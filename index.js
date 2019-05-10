c7eef8f7c55aeda47a31a5c4c942a5a777472f46
Zwe Mun Htun <zwemunhtun@ucsy.edu.mm>
2019-05-10 17:28:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Hello, Neil deGrasse Tyson!"
+var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello" + specialGuest + "Neil deGrasse Tyson!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
